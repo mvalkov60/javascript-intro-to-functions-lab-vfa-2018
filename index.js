0ba8ca18203649e0bbdd8f934b331fd6e7b99ceb
Martin Valkov <mvalkov60@gmail.com>
2018-06-25 00:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ function logShout(string){
 
 function sayHiToGrandma(string){
    if (string == string.toLowerCase) {
-     
+     return "I can't hear you!"
    }
-   return "I can't hear you!"
+   
+   
    else if (string == string.toUpperCase) {
-     
+     return "YES INDEED!";
    }
-   return "YES INDEED!"
+   
   }
   
